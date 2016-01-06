7863b64bc4ffd931ec799e5c60d4c679cbb904fb
DanyTlaw <daniel.herzogfhnw@gmail.com>
2016-01-06 17:39:37 +0100

    verschiedene seiten

--- juraprojekt/config/routes.rb
+++ juraprojekt/config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
  
-    root "main#home"
+    root "main#site1"
+    get '/site1' => 'main#site1'
+    get '/site2' => 'main#site2'
+    get '/site3' => 'main#site3'
+    get '/site4' => 'main#site4'
+    get '/site5' => 'main#site5'
+    get '/site6' => 'main#site6'
+    get '/site7' => 'main#site7'
+    get '/site8' => 'main#site8'
+    get '/site9' => 'main#site9'
+    get '/site10' => 'main#site10'
+    get '/site11' => 'main#site11'
+    get '/site12' => 'main#site12'
 
 end
